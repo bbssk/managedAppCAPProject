f3bd8b3dba1b6e722089765317f7a97859892fa8
BHARGAVA SRI SAI KRISHNA BUDDANA <bbsskrishna@gmail.com>
2025-05-06 09:10:57 +0000

    added fori app

---
+++ app/feproject-ui/dist/Component.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("cap.tutorial.feprojectui.Component",{metadata:{manifest:"json"}})});
+//# sourceMappingURL=Component.js.map
